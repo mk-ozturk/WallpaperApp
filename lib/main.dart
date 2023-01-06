5b431e6f7c264c0bc6edd78019b57aa3d64a0b75
Mustafa Kemal Ozturk <kemal7991@hotmail.com>
2023-01-07 02:30:49 +0300

    api deneniyor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart'as http;
+import 'package:walpaper_app/Parsecode.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,55 +18,86 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
 
 
-  final String title;
+
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  String apiKey="32652039-5de9198ed878c2c8b355d4561";
+
+ List<Hit> parseImgs(String cevap){
+   return ImgParse.fromJson(jsonDecode(cevap)).hits;
+
+   }
+
+   Future<List<Hit>> search(String id) async{
+
+   var url=Uri.parse("https://pixabay.com/api/?key=$apiKey&q=$id");
+    var cevap= await http.get(url);
+
+   return parseImgs(cevap.body);
+
+   }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Yugioh"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: search("space+wallpaper"),
+        builder: (context, AsyncSnapshot<List<Hit>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            debugPrint("başarıyla baglanıldı");
+            var imgdata=snapshot.data;
+            return GridView.builder(gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                ),
+                itemCount: imgdata?.length,
+                itemBuilder: (context, i){
+              var images=imgdata![i];
+              return Card(child: Image.network(images.webformatUrl),) ;
+
+
+                });
+          } else {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+        },
       ),
+      //floataction button ekleyip kategorilere bir bak.
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          String smr="Summer";
+         search(smr);
+
+        },
+        backgroundColor: Colors.green,
+        child: Text("Summer"),
       ),
     );
   }
