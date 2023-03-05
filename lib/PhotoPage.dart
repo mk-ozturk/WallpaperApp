c17cde8f9c302917ee156b71469cc6195b951264
Mustafa Kemal Ozturk <kemal7991@hotmail.com>
2023-03-05 22:28:39 +0300

    complate download

--- lib/PhotoPage.dart
+++ lib/PhotoPage.dart
@@ -92,7 +92,7 @@ class Imgdetail extends StatelessWidget {
               value: 2,),
             PopupMenuItem(child: Text(lang==true ?"Set as Lock Screen and Home Screen":"Kilit ve Arka Plan Olarak Ayarla "),
               value: 3,),
-            PopupMenuItem(child: Text(lang==true ?"Downloa":"İndir"),
+            PopupMenuItem(child: Text(lang==true ?"Download":"İndir"),
               value: 4,)
 
           ],
